9a1635a024e25c182ea97e5643b274ccc86553f2
Frans Engström <frans.engstrom@learnet.se>
2023-05-15 19:23:15 +0200

    update product row

--- src/components/ProductRow.tsx
+++ src/components/ProductRow.tsx
@@ -1,14 +1,15 @@
 interface Props {
   name: string;
   expirationDays: number;
+  onClick: () => void;
 }
 
-function ProductRow({ name, expirationDays }: Props) {
+function ProductRow({ name, expirationDays, onClick }: Props) {
   return (
-    <>
+    <tr onClick={onClick}>
       <td>{name}</td>
       <td>{expirationDays}</td>
-    </>
+    </tr>
   );
 }
 export default ProductRow;
