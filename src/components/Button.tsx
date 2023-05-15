7d890a8530c701dc1ff9d1e3c9f3b05cb3b3f4ae
Frans Engström <frans.engstrom@learnet.se>
2023-05-15 21:19:42 +0200

    add loading spinner

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,16 +8,17 @@ interface Props {
   disabled?: boolean;
   class?: string;
   variant?: "primary" | "secondary" | "danger";
+  loading?: boolean;
 }
 
-function Button({
-  children,
-  type = "button",
-  onClick,
-  disabled,
-  class: className,
-  variant = "primary",
-}: Props) {
+function Button(props: Props) {
+  const {
+    type = "button",
+    onClick,
+    disabled,
+    class: className,
+    variant = "primary",
+  } = props;
   return (
     <button
       type={type}
@@ -31,7 +32,30 @@ function Button({
         className
       )}
     >
-      {children}
+      {props.loading ? (
+        <svg
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      ) : (
+        props.children
+      )}
     </button>
   );
 }
