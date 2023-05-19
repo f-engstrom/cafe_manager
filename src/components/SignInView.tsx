ddf0894a4d415656a2c60977c8855a9089ed640a
Frans Engström <frans.engstrom@learnet.se>
2023-05-19 16:23:34 +0200

    update signin view

--- src/components/SignInView.tsx
+++ src/components/SignInView.tsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "@solidjs/router";
 import { supabase } from "../lib/supabase";
-import { setSession } from "../App";
+import { useAuth } from "./AuthContext";
 
 function SignInView() {
   const navigate = useNavigate();
-
+  const [session, setSession] = useAuth();
   const signIn = async (event: Event) => {
     event.preventDefault();
     const {
@@ -18,12 +18,64 @@ function SignInView() {
     navigate("/", { replace: true });
   };
   return (
-    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-      <form class="flex flex-col gap-4" onSubmit={signIn}>
-        <input type="text" placeholder="email" name="email" />
-        <input type="password" placeholder="lösenord" name="password" />
-        <button type="submit">logga in</button>
-      </form>
+    <div class="flex min-h-full flex-col justify-center pt-24 pb-12 sm:px-6 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Logga in på ditt Cafe Manager konto
+        </h2>
+      </div>
+
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <form class="space-y-6" onSubmit={signIn}>
+            <div>
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                for="password"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Lösenord
+              </label>
+              <div class="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="current-password"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Logga in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
