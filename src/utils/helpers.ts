3adedff23482172f5dc8b0f2338cf2732eba1b27
Frans Engström <frans.engstrom@learnet.se>
2023-05-19 16:20:14 +0200

    add helpers for timeout and sorting

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -12,3 +12,32 @@ export const setFutureDate = (date: string, days: number) => {
   dateObj.setDate(dateObj.getDate() + days);
   return dateObj.toISOString().split("T")[0];
 };
+
+export const timeOut = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const sortAndGroup = <
+  T extends { products?: { product_name?: string }[] }
+>(
+  data: T[]
+): { name: string; products: T[] }[] => {
+  if (!data) return [];
+  return data.reduce<{ name: string; products: T[] }[]>((acc, curr) => {
+    console.log(curr);
+    let product = "";
+    if (curr.products?.product_name) {
+      product = curr.products.product_name;
+    } else {
+      product = "Okänd produkt";
+    }
+    const existingProduct = acc.find((p) => p.name === product);
+    if (!existingProduct) {
+      let productGroup = { name: product, products: [curr] };
+      acc.push(productGroup);
+    } else {
+      existingProduct.products.push(curr);
+    }
+    return acc;
+  }, []);
+};
