b4d1dd4fad4af0dec2c9c59665e49d19a3c0e86d
Frans Engström <frans.engstrom@learnet.se>
2023-05-19 16:28:59 +0200

    restyle rows

--- src/components/ProductRow.tsx
+++ src/components/ProductRow.tsx
@@ -6,9 +6,13 @@ interface Props {
 
 function ProductRow({ name, expirationDays, onClick }: Props) {
   return (
-    <tr onClick={onClick}>
-      <td>{name}</td>
-      <td>{expirationDays}</td>
+    <tr onClick={onClick} class="hover:bg-purple-200 border-t border-gray-300">
+      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+        {name}
+      </td>
+      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+        {expirationDays}
+      </td>
     </tr>
   );
 }
