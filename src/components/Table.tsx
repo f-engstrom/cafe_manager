f6d065f78a189a6b50ff89135a86111c2a433838
Frans Engström <frans.engstrom@learnet.se>
2023-06-04 22:19:12 +0200

    make table only scroll

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -14,8 +14,8 @@ interface Props {
 export default function Table(props: Props) {
   const { onClick, heading, buttonText, tableHeadings } = props;
   return (
-    <div class="mx-10 mt-8 flex max-h-screen flex-col bg-white">
-      <div class="mb-6 flex justify-between  bg-white">
+    <div class="ml-5 mt-8 flex max-h-screen flex-col overflow-hidden bg-white">
+      <div class="mx-10 mb-6 flex  justify-between bg-white">
         <h2 class=" bold text-2xl">{heading}</h2>
         <Button variant="primary" displayWidth="auto" onClick={onClick}>
           {buttonText}
@@ -25,22 +25,24 @@ export default function Table(props: Props) {
         when={!props.loading}
         fallback={<LoadingSpinner className="mt-8" />}
       >
-        <table class="table-auto ">
-          <thead>
-            <tr class="bg-white">
-              <For each={tableHeadings}>
-                {(heading) => {
-                  return (
-                    <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
-                      {heading}
-                    </th>
-                  );
-                }}
-              </For>
-            </tr>
-          </thead>
-          <tbody class="overflow-auto">{props.rows}</tbody>
-        </table>
+        <div class="mb-10 max-h-fit overflow-auto">
+          <table class="w-full border-collapse">
+            <thead class="sticky top-0">
+              <tr class="bg-white">
+                <For each={tableHeadings}>
+                  {(heading) => {
+                    return (
+                      <th class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                        {heading}
+                      </th>
+                    );
+                  }}
+                </For>
+              </tr>
+            </thead>
+            <tbody class="overflow-auto">{props.rows}</tbody>
+          </table>
+        </div>
       </Show>
     </div>
   );
